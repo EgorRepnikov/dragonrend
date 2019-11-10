f831e476b5e1e752bad26362559efdb567d6e194
EgorRepnikov <egorepnikov@gmail.com>
2019-11-10 16:38:53 +0300

    Add describe blocks to auto-including-tasks tests

--- test/auto-including-tasks/index.server.test.js
+++ test/auto-including-tasks/index.server.test.js
@@ -6,7 +6,7 @@ const rp = require('request-promise').defaults({
 
 const Dragonrend = require('../../lib/Dragonrend')
 
-describe('Routes Auto Including', () => {
+describe('Auto Including Tasks', () => {
   const dragonrend = new Dragonrend({
     routesDir: 'test/auto-including-tasks/routes',
     middlewareDir: 'test/auto-including-tasks/middleware',
@@ -16,38 +16,44 @@ describe('Routes Auto Including', () => {
   beforeAll(async () => await dragonrend.start(8080))
   afterAll(async () => await dragonrend.stop())
 
-  it('GET /router1 (Router1)', async () => {
-    const res = await rp('router1')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual('router1')
-  })
-
-  it('GET /router2 (Router2)', async () => {
-    const res = await rp('router2')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual('router2')
-  })
-
-  it('GET /middleware1 (middleware1)', async () => {
-    const res = await rp('router1?middleware1=lel')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual('middleware1')
-  })
-
-  it('GET /middleware2 (middleware2)', async () => {
-    const res = await rp('router2?middleware2=kek')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual('middleware2')
-  })
-
-  it('GET /parser (textPlain)', async () => {
-    const res = await rp.post('parser', {
-      headers: {
-        'content-type': 'text/plain'
-      },
-      body: 'hi there'
+  describe('Routers', () => {
+    it('GET /router1 (Router1)', async () => {
+      const res = await rp('router1')
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual('router1')
     })
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toEqual('HI THERE')
+
+    it('GET /router2 (Router2)', async () => {
+      const res = await rp('router2')
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual('router2')
+    })
+  })
+
+  describe('Middleware', () => {
+    it('GET /middleware1 (middleware1)', async () => {
+      const res = await rp('router1?middleware1=lel')
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual('middleware1')
+    })
+
+    it('GET /middleware2 (middleware2)', async () => {
+      const res = await rp('router2?middleware2=kek')
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual('middleware2')
+    })
+  })
+
+  describe('Content Type Parsers', () => {
+    it('GET /parser (textPlain)', async () => {
+      const res = await rp.post('parser', {
+        headers: {
+          'content-type': 'text/plain'
+        },
+        body: 'hi there'
+      })
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toEqual('HI THERE')
+    })
   })
 })
